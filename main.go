9d019c0c3c1c72419d998f21e612b09c0b8c3c1c
darkline28 <Dorokhin.d28gmail.com>
2025-10-21 11:13:54 +0300

    Add GORM version

--- main.go
+++ main.go
@@ -2,118 +2,157 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
 	"strconv"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 type Task struct {
-	ID     int    `json:"id"`
+	ID     int    `gorm:"primaryKey" json:"id"`
 	Text   string `json:"text"`
 	Status string `json:"status"`
 }
 
 type Server struct {
-	Tasks  map[int]Task
-	NextID int
+	Db *gorm.DB
 }
 
 type ServerReq struct {
 	Task string `json:"task"`
 }
 
+func initDB() (*gorm.DB, error) {
+	dsn := "host=localhost user=project2_user password=pass2 dbname=project2_db port=5433 sslmode=disable"
+	var err error
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	if err = db.AutoMigrate(&Task{}); err != nil {
+		return nil, err
+	}
+	return db, nil
+
+}
+
 func (s *Server) handlerTasks(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
 
 	switch r.Method {
 	case http.MethodGet:
-		// вернуть все задачи (массив, не map)
-		tasks := make([]Task, 0, len(s.Tasks))
-		for _, t := range s.Tasks {
-			tasks = append(tasks, t)
+		var tasks []Task
+
+		if err := s.Db.Find(&tasks).Error; err != nil {
+			writeJSONError(w, http.StatusInternalServerError, "could not fetch tasks")
+			return
 		}
+		w.Header().Set("Content-Type", "application/json")
+		if len(tasks) == 0 {
+			json.NewEncoder(w).Encode([]Task{})
+			return
+		}
+
 		json.NewEncoder(w).Encode(tasks)
+		return
 
 	case http.MethodPost:
-		// создать новую задачу
 		var req struct {
 			Text string `json:"text"`
 		}
 		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-			http.Error(w, `{"error":"bad request"}`, http.StatusBadRequest)
+			writeJSONError(w, http.StatusBadRequest, "bad request")
 			return
 		}
+		defer r.Body.Close()
 		if req.Text == "" {
-			http.Error(w, `{"error":"text required"}`, http.StatusBadRequest)
+			writeJSONError(w, http.StatusBadRequest, "text required")
 			return
 		}
 
 		task := Task{
-			ID:     s.NextID,
 			Text:   req.Text,
 			Status: "new",
 		}
-		s.Tasks[s.NextID] = task
-		s.NextID++
-
+		if err := s.Db.Create(&task).Error; err != nil {
+			writeJSONError(w, http.StatusInternalServerError, "could not create task")
+			return
+		}
 		w.Header().Set("Location", "/tasks/"+strconv.Itoa(task.ID))
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusCreated)
 		json.NewEncoder(w).Encode(task)
 
 	default:
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		writeJSONError(w, http.StatusMethodNotAllowed, "method not allowed")
+		return
 	}
 }
 func (s *Server) handlerTaskByID(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
 	idStr := r.URL.Path[len("/tasks/"):]
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
-		http.Error(w, `{"error":"invalid id"}`, http.StatusBadRequest)
+		writeJSONError(w, http.StatusBadRequest, "invalid id")
 		return
 	}
-
-	task, ok := s.Tasks[id]
-	if !ok {
-		http.Error(w, `{"error":"task not found"}`, http.StatusNotFound)
+	var task Task
+	if err := s.Db.First(&task, id).Error; err != nil {
+		writeJSONError(w, http.StatusNotFound, "task not found")
 		return
 	}
 
 	switch r.Method {
-
-	case http.MethodGet:
-		json.NewEncoder(w).Encode(task)
-
 	case http.MethodPatch:
 		var req Task
 		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-			http.Error(w, `{"error":"bad request"}`, http.StatusBadRequest)
+			writeJSONError(w, http.StatusBadRequest, "bad request")
 			return
 		}
+		defer r.Body.Close()
 		if req.Text != "" {
 			task.Text = req.Text
 		}
 		if req.Status != "" {
 			task.Status = req.Status
 		}
-		s.Tasks[id] = task
+
+		if err := s.Db.Save(&task).Error; err != nil {
+			writeJSONError(w, http.StatusInternalServerError, "could not update task")
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(task)
 
 	case http.MethodDelete:
-		delete(s.Tasks, id)
+		if err := s.Db.Delete(&task).Error; err != nil {
+			writeJSONError(w, http.StatusInternalServerError, "could not delete task")
+			return
+		}
 		w.WriteHeader(http.StatusNoContent)
-
 	default:
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		writeJSONError(w, http.StatusMethodNotAllowed, "Method not allowed")
 	}
 }
 
 func main() {
-	srv := &Server{
-		Tasks:  make(map[int]Task),
-		NextID: 1,
+	db, err := initDB()
+	if err != nil {
+		fmt.Printf("Database error: %v", err)
+		os.Exit(1)
 	}
+	srv := &Server{Db: db}
 	http.HandleFunc("/tasks", srv.handlerTasks)
 	http.HandleFunc("/tasks/", srv.handlerTaskByID)
 	http.ListenAndServe(":8080", nil)
+}
+
+func writeJSONError(w http.ResponseWriter, code int, msg string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(map[string]string{
+		"error":   http.StatusText(code),
+		"message": msg,
+	})
 }
